24a490cb6d556f78ac0eedb6f1ea41bdbea30853
Andrew Nguyen <info@andrewnguyen.ca>
2015-01-29 15:44:12 -0600

    Modular code, drools.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,15 +1,43 @@
 $(function() {
 
-	// This will show the credits
-	$(".new").fadeOut("1000");
-	$(".credits").fadeIn("1000");
+	// Trying to figure out how to Dynamo this
+	$(".credits").hide();
 
 	// This hides the footer on click
 	$(".crossContainer").click(function(){
 		$("footer").slideUp("slow", function(){
 			console.log("I'm a ninja!");
-		})
+		});
 	});
+
+	// For a pie chart
+	var countries = document.getElementById("myChart").getContext("2d");
+	new Chart(countries).Pie(pieData, pieOptions);
+
+	var pieData = [
+		{
+			value: 20,
+			color:"#878BB6"
+		},
+		{
+			value : 40,
+			color : "#4ACAB4"
+		},
+		{
+			value : 10,
+			color : "#FF8153"
+		},
+		{
+			value : 30,
+			color : "#FFEA88"
+		}
+	];
+
+
+	var pieOptions = {
+		segmentShowStroke : false,
+		animateScale : true
+	}
 });
 
 
